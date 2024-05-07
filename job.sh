936cde78efd0927b4a154b6cdf8df2b36b55111a
Eiji Takahashi <comu2e@gmail.com>
2024-05-07 13:31:08 +0900

    fix:shell

--- job.sh
+++ job.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-BATCH=20
-sh launch.sh $((BATCH + 1)) "us-east1" "crawler2"
-sh launch.sh $((BATCH + 2)) "us-central1" "crawler-uscentral"
-sh launch.sh $((BATCH + 3)) "us-west1" "crawler-uswest"
-sh launch.sh $((BATCH +4)) "us-east4" "crawler-useast4"
+# 定義済みのリージョンと名前
+REGIONS=("us-east1" "us-central1" "us-west1")
+NAMES=("crawler2" "crawler-uscentral" "crawler-uswest")
+
+# launch関数
+launch() {
+    local batch=$1
+    local region=$2
+    local name=$3
+    sh launch.sh "$batch" "$region" "$name"
+    echo "$batch" "$region" "$name"
+}
+
+# 100から2002までループし、各バッチでリージョンと名前を繰り返す
+BATCH_START=24
+BATCH_END=36
+region_count=${#REGIONS[@]}
+
+for (( batch=$BATCH_START; batch<=$BATCH_END; batch++ )); do
+    # インデックスを使ってREGIONSとNAMESを順番に取得する
+    index=$(( (batch - BATCH_START) % region_count ))
+    launch "$batch" "${REGIONS[$index]}" "${NAMES[$index]}"
+done
